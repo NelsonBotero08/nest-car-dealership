2b4405a5f680a147c3ffdbf620c5bd5721d96ae6
NelsonBotero1518 <nelsonkennedy2004@gmail.com>
2025-04-26 11:41:12 -0500

    practicando el crud se hace post

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,37 +1,55 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateCarDto } from './dto/create-carDto.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class CarsService {
+  private cars = [
+    {
+      id: uuid(),
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: uuid(),
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: uuid(),
+      brand: 'Jeep',
+      model: 'Cherokee',
+    },
+  ];
 
-    private cars = [
-        {
-            id: 1,
-            brand: 'Toyota',
-            model: 'Corolla' 
-        },
-        {
-            id: 2,
-            brand: 'Honda',
-            model: 'Civic' 
-        },
-        {
-            id: 3,
-            brand: 'Jeep',
-            model: 'Cherokee' 
-        },
-    ];
+  findAll() {
+    return this.cars;
+  }
 
+  findOneById(id: string) {
+    const car = this.cars.find((car) => car.id === id);
+    if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
 
-    findAll() {
-        return this.cars;
-    }
+    return car;
+  }
 
-    findOneById( id: number ) {
-        
-        const car = this.cars.find( car => car.id === id );
-        if ( !car ) throw new NotFoundException(`Car with id '${ id }' not found`);
-        
-        return car;
-    }
+  create(createCarDto: CreateCarDto) {
+    const { brand, model } = createCarDto;
 
+    const car = this.cars.find(
+      (car) => car.brand === brand && car.model === model,
+    );
+
+    if (car) throw new NotFoundException('this car already exists');
+
+    const newcar = {
+      id: uuid(),
+      brand,
+      model,
+    };
+
+    this.cars.push(newcar);
+
+    return newcar;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // elimina propiedades que no están en el DTO
+      forbidNonWhitelisted: true, // lanza error si hay propiedades no permitidas
+      transform: true, // transforma payloads a instancias de clases DTO
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
